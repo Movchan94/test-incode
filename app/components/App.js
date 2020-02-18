d805cb38dcc1a41406a01d780247402b90b6073a
Movchan94 <tkd.movchan@gmail.com>
2020-02-18 22:46:39 +0200

    Added modules and created new components TickerPricew

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import '../styles/application.scss';
 import {connect} from '../services';
 import React, {PureComponent} from 'react';
+import TickerPrice from "./TickerPrice";
 
 // The below line is here as an example of getting prices
 connect('AAPL');
@@ -10,6 +11,7 @@ class App extends PureComponent {
         return (
             <div className="stock-ticker">
                 <h1>Stock Blotter</h1>
+                <TickerPrice/>
 
 
             </div>
---
+++ app/components/TickerPrice.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+
+class TickerPrice extends React.Component {
+    render() {
+        return (
+            <div>
+
+               Ticker price
+
+            </div>
+        );
+    }
+}
+
+export default TickerPrice;
